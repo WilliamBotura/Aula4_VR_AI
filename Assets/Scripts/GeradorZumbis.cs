159aec9a903382e7df00b1ed2f8284534b1e63f2
William Botura <williamdigitalwork@gmail.com>
2019-04-02 09:40:55 -0300

    Aula 02_04
    Morte do jogador adicionada, programação do gberador de zumbis, correção de bugs básicos e polimento inicial

--- Assets/Scripts/GeradorZumbis.cs
+++ Assets/Scripts/GeradorZumbis.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class GeradorZumbis : MonoBehaviour
 {
+    //variaveis e objetos
+    public float delay = 2.0f;
+    public GameObject zumbiPrefab;
+    private float area = 3.0f;
+
     private void Start()
     {
+        //chama um metodo repetidamente por determinado tempo
+        InvokeRepeating("GerarZumbi", delay, delay);
     }
 
-    private void Update()
+    void GerarZumbi()
     {
+        //instanciando o prefab do zumbi
+        GameObject zumbi = Instantiate(zumbiPrefab);
+        //criando um vector 2 (x, y) em área de um circulo, e multiplicando para aumentar o raio
+        Vector2 posicaoAleatoria = Random.insideUnitCircle * area;
+        //criar um vector 3 de localização do zumbi dentro do raio permitido pelo circulo
+        zumbi.transform.position = new Vector3(posicaoAleatoria.x, zumbi.transform.position.y, posicaoAleatoria.y);
+
     }
 }
--- Assets/Scripts/Zumbi.cs
+++ Assets/Scripts/Zumbi.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Zumbi : MonoBehaviour
 {
     private Animator anim;
     private Rigidbody rb;
+    GameObject jogador;
 
     public float vel = 0.35f;
+    bool morto = false;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
+        jogador = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void Update()
@@ -26,20 +30,45 @@ public class Zumbi : MonoBehaviour
 
         //compara se o tag do que atinge o Trigger é igual a do projetil, se for, destroi o projetil
         //e o player
-        if (col.CompareTag("proj"))
+        if (!morto && col.CompareTag("proj"))
         {
             Destroy(col.gameObject);
             //Destroy(gameObject);
 
-            //set no Animator o trigger de morrer como verdadeiro
-            anim.SetTrigger("Morrer");
+            Morrer();
         }
+    }
+
+    private void Morrer()
+    {
+        //set no Animator o trigger de morrer como verdadeiro
+        anim.SetTrigger("Morrer");
+
+        morto = true;
     }
 
     //movimentação do zumbi com proprio metodo
     private void Movimentacao()
     {
         //o transform pega a rotação do zumbi, enquanto o Vector3 pega a a rotação do do mundo
-        rb.velocity = transform.forward * vel;
+        if (!morto)
+        {
+            //move o zumbi
+            rb.velocity = transform.forward * vel;
+            //zumbu sempre olha para o jogador
+            transform.LookAt(jogador.transform);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            //armazena em uma variavel a cena atual
+            Scene currentScene = SceneManager.GetActiveScene();
+            //mudado para o scene manager a partir do alt+enter, 
+            SceneManager.LoadScene(currentScene.buildIndex);
+        }
     }
 }
